f75ae0faa66a86c42cb035b4f61554d644ea560c
kamilfocus <k4mil.piszczek@gmail.com>
2015-03-20 21:36:56 +0100

    projektwre

--- app/src/main/java/modernart/labs/course/modernart/ModernArtActivity.java
+++ app/src/main/java/modernart/labs/course/modernart/ModernArtActivity.java
@@ -1,9 +1,20 @@
 package modernart.labs.course.modernart;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.InflateException;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 
 public class ModernArtActivity extends ActionBarActivity {
@@ -12,13 +23,31 @@ public class ModernArtActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modern_art);
+
+        SurfaceView leftUpTile = (SurfaceView) findViewById(R.id.leftUpTile);
+        SurfaceView leftDownTile = (SurfaceView) findViewById(R.id.leftDownTile);
+        SurfaceView rightUpTile = (SurfaceView) findViewById(R.id.rightUpTile);
+        SurfaceView rightMidTile = (SurfaceView) findViewById(R.id.rightMidTile);
+        SurfaceView rightDownTile = (SurfaceView) findViewById(R.id.rightDownTile);
+
+        SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+
+        leftUpTile.setBackgroundColor(Color.BLUE);
+        leftDownTile.setBackgroundColor(Color.RED);
+        rightUpTile.setBackgroundColor(Color.YELLOW);
+        rightMidTile.setBackgroundColor(Color.MAGENTA);
+        rightDownTile.setBackgroundColor(Color.GREEN);
+
+
+
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_modern_art, menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_modern_art, menu);
         return true;
     }
 
@@ -37,3 +66,5 @@ public class ModernArtActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+
+
